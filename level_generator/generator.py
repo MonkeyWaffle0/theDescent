907a2663c342fac260dc47733a59cb04fb1feb0f
monkeywaffle0 <rea.florian@gmail.com>
2020-11-11 17:46:17 -0500

    generator

---
+++ level_generator/generator.py
@@ -0,0 +1,36 @@
+import pygame
+
+from data.scripts.ui.user_input import InputManager
+
+from data.scripts.window import GameWindow
+
+from data.scripts.config import FPS
+from level_generator.data.scripts.scenes.GeneratorScene import GeneratorScene
+
+
+class LevelGenerator:
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+        pygame.init()
+        pygame.display.set_caption('level generator')
+
+        self.window = GameWindow()
+        self.input = InputManager(self)
+        self.clock = pygame.time.Clock()
+
+        self.active_scene = GeneratorScene(self)
+
+        self.game_timer = 0
+
+    def update(self):
+        self.active_scene.handle_game_frame()
+        self.window.update(self)
+        self.clock.tick(FPS)
+
+    def run(self):
+        while self.active_scene is not None:
+            self.update()
+
+
+if __name__ == '__main__':
+    LevelGenerator().run()
